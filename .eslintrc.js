d03e3716ac0991bc3cd0941ec8899a72af74067f
eurico77 <euricomagalhaes61@gmail.com>
2022-07-23 10:16:04 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    'react-native/no-inline-styles': 'off',
+    'react-native/no-color-literals': 'off',
+    'react-native/no-raw-text': 'off',
+    'react-native/no-styles': 'off',
+    'react-native/no-unused-styles': 'off',
+  },
+};
