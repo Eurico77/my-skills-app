3cef463be720c63705414aceef4f5139c2f0a501
eurico77 <euricomagalhaes61@gmail.com>
2022-07-24 05:56:34 -0300

    feat: create component title

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+export function Title({children}) {
+  return <Text style={styles.text}>{children}</Text>;
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#fff',
+    alignSelf: 'center',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
