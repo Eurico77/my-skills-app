093a85050ecc9e926cbc99a6268c6e54f4585b92
eurico77 <euricomagalhaes61@gmail.com>
2022-07-24 12:22:00 -0300

    refactor: adjutment rules of prettier

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,14 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import App from '../App';
 
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const app = renderer.create(<App />);
+  expect(app).toMatchSnapshot();
+
+  const input = app.root.findByType('TextInput');
+  input.props.onChangeText('Hello');
+  expect(app).toMatchSnapshot();
 });
